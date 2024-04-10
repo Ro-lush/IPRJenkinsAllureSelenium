ac595a1cfaf4d75f6a10805f4f80c07e82f899a5
Roman <aevtann28@gmail.com>
2024-04-10 21:45:48 +0400

    fix BaseCore and pom.xml(junit-engine)

--- src/test/java/Base/BaseCore.java
+++ src/test/java/Base/BaseCore.java
@@ -1,7 +1,7 @@
 package Base;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,17 +12,18 @@ abstract public class BaseCore {
     public static WebDriver driver;
     public static WebDriverWait driverWait;
 
-    @BeforeEach
-    public void startDriver(){
-        System.setProperty(ConfigReader.DRIVER_NAME, ConfigReader.CHROME_DRIVER_MAC);
+    @BeforeAll
+    public static void startDriver(){
+        //  System.setProperty(ConfigReader.DRIVER_NAME, ConfigReader.CHROME_DRIVER_WIN); // пример записи указывает имя дрйвера и путь до драйвера
+        // в новой версии selenium скачивает все нужные драйвера сам.
         driver = new ChromeDriver();
         driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));//Устанавливает время ожидания завершения загрузки страницы перед выдачей ошибки.
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));//Указывает время, в течение которого драйвер должен ждать при поиске элемента, если он отсутствует сразу.
 
     }
-    @AfterEach
-    public void closeDriver(){
+    @AfterAll
+    public static void closeDriver(){
         driver.close();
         driver.quit();
     }
