00fe44724c0c00ced7b2c8df2c66998313b5b18d
romanlusnikov <aectann28@gmail.com>
2024-04-10 16:27:19 +0400

    new day

--- src/test/java/TestIPR.java
+++ src/test/java/TestIPR.java
@@ -2,10 +2,10 @@ import Base.BaseCore;
 import Base.ConfigReader;
 import org.junit.jupiter.api.Test;
 
-public class thisIsTest extends BaseCore {
+public class TestIPR extends BaseCore {
 
     @Test
-   public void testGoogleSerch(){
+   public void GoogleSerch(){
         driver.get(ConfigReader.URL);
 
     }
